ced49e1d4d68368664b67fd08c3cc4098ed2c090
t.mesropyan <tatul.mesropyan-extern@invia.de>
2022-06-12 01:11:03 +0400

    Featerd new structure for RC template page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import React from 'react';
 import Punchin from './Punchin';
 import Header from './Header.jsx'
 import LoginPage from './LoginPage';
-import RateCon from './RateCon'
+import RateCon from './RateconTemplate/RateCon'
+import RateConHeader from "./RateconTemplate/RateconHeader/RateConHeader";
 function App() {
+    const headerData = {
+        brokerName:'',
+        brokerPhone:'',
+        brokerEmail:'',
+    }
   return (
    <>
-  <RateCon/>
+       <RateCon headerData={headerData}/>
    </>
   );
 }
